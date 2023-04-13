d7d50702c60b272611833c759ebfe72911de1ed2
mia <miabenn74@gmail.com>
2023-04-13 10:46:38 +1000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Mia on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
